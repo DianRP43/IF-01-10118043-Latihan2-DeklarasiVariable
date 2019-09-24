cf7a4effdea59fe71ec8a10d968c31aa962cecf8
DianRP43 <55683288+DianRP43@users.noreply.github.com>
2019-09-24 14:57:57 +0700

    Add files via upload
    PBO1 = IF-01

---
+++ PBO1-10118043-Latihan2-DeklarasiVariable/src/pbo1/pkg10118043/latihan2/deklarasivariable/PBO110118043Latihan2DeklarasiVariable.java
@@ -0,0 +1,39 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package pbo1.pkg10118043.latihan2.deklarasivariable;
+
+/**
+ *
+ * @author user
+ */
+public class PBO110118043Latihan2DeklarasiVariable {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+        int NilaiInt;
+        final double PHI = 3.14;
+        boolean NilaiLogika;
+        char NilaiKarakter;
+        
+        NilaiInt = 78;
+        NilaiLogika = false;
+        NilaiKarakter = 'B';
+        
+        System.out.println("");
+        System.out.println("Isi Variable Nilai\t= " + NilaiInt);
+        System.out.println("Isi Variable PHI\t= " + PHI);
+        System.out.println("Isi Variable Logika\t= " + NilaiLogika);
+        System.out.println("Isi Variable Karakter\t= " + NilaiKarakter);
+                
+                
+      
+        
+    }
+    
+}
